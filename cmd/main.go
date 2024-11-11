d4c07b94847de32dbc931ce0e079e97d7c65fed7
Regular-Pashka <paxa39ru@gmail.com>
2024-11-12 02:23:48 +0300

    psql migration

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/Regular-Pashka/todo-app"
 	"github.com/Regular-Pashka/todo-app/pkg/handler"
 	"github.com/Regular-Pashka/todo-app/pkg/repository"
@@ -10,12 +11,23 @@ import (
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs $s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}	
 }
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
+//migrate -path ./schema -database 'postgres://postgres:qwert@localhost:5436/postgres?sslmode=disable' up
