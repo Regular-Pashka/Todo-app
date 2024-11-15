f68db171cc7cc1b39ef413ad91f0a60f2dbbee31
Regular-Pashka <paxa39ru@gmail.com>
2024-11-15 22:29:54 +0300

    jwt auth done

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -6,7 +6,7 @@ import (
 )
 
 type err struct {
-	Message string `json"message"`
+	Message string `json:"message"`
 }	
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,23 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
 	"github.com/Regular-Pashka/todo-app"
 	"github.com/Regular-Pashka/todo-app/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "fsajflsajflk423fs3r23fs"
+const (
+	salt = "fsajflsajflk423fs3r23fs"
+	signingKey = "fasjfh3rjha3h23424sfa3423"
+	tokenTTL = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +32,24 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	// get user from DB
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt: time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
