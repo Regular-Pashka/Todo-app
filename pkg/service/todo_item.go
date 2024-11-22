350fc4ccefbcf5bf278e2b2fbca0ac8915aa8fee
Stacie Sudie <staciesu@il-i4.msk.21-school.ru>
2024-11-22 20:26:58 +0300

    delete handler done

--- pkg/service/todo_item.go
+++ pkg/service/todo_item.go
@@ -31,6 +31,10 @@ func (s *TodoItemService) GetById(userId, itemId int) (todo.TodoItem, error) {
 	return s.repo.GetById(userId, itemId)
 }
 
-func (s *TodoItemService) Delete(userId,itemId int) error {
+func (s *TodoItemService) Delete(userId, itemId int) error {
 	return s.repo.Delete(userId, itemId)
+}
+
+func (s *TodoItemService) Update(userId, itemId int, input todo.UpdateItemInput) error {
+	return s.repo.Update(userId, itemId, input)
 }
