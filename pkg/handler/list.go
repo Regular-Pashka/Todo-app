41f2326b3dc81171189ffc385cd41f23d17e03b8
Regular-Pashka <paxa39ru@gmail.com>
2024-12-01 22:26:00 +0300

    swagger in progress

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -85,7 +85,7 @@ func (h *Handler) getAllLists(c *gin.Context) {
 // @ID get-list-by-id
 // @Accept  json
 // @Produce  json
-// @Success 200 {object} todo.ListItem
+// @Success 200 {object} todo.ListsItem
 // @Failure 400,404 {object} errorResponse
 // @Failure 500 {object} errorResponse
 // @Failure default {object} errorResponse
